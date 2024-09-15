1b6594af220ee8f117e64ddee280f9ea2e7984ea
405596-MorenoTobias <405596@tecnicatura.frc.utn.edu.ar>
2024-09-15 11:50:42 -0300

    update gitignore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { API_KEY, APP_ID, AUTH_DOMAIN, MESSAGING_SENDER_ID, PROJECT_ID, STORAGE_BUCKER } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,12 +13,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'crud-auth-adb7a',
-        appId: '1:368944034448:web:44f148d61a112c25e19e15',
-        storageBucket: 'crud-auth-adb7a.appspot.com',
-        apiKey: 'AIzaSyDRI7hBCOm5CSS5hKg25BfvilO8ULDt9_M',
-        authDomain: 'crud-auth-adb7a.firebaseapp.com',
-        messagingSenderId: '368944034448',
+        projectId: PROJECT_ID,
+        appId: APP_ID,
+        storageBucket: STORAGE_BUCKER,
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        messagingSenderId: MESSAGING_SENDER_ID,
       })
     ),
     provideAuth(() => getAuth()),
